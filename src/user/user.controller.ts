5a95538e2e10578b70d3036992a5cc62f909f4d9
Martin <mselvenis@gmail.com>
2021-05-30 23:20:22 +0100

    17. Creating Users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { AuthGuard } from './../auth/auth.guard';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { User } from './models/user.entity';
 import { UserService } from './user.service';
+import * as bcrypt from 'bcryptjs';
+import { UserCreateDto } from './models/user-create.dto';
 
+@UseGuards(AuthGuard)
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
@@ -9,5 +13,17 @@ export class UserController {
   @Get()
   async all(): Promise<User[]> {
     return this.userService.all();
+  }
+
+  @Post()
+  async create(@Body() body: UserCreateDto): Promise<User> {
+    const password = await bcrypt.hash('1234', 12);
+
+    return this.userService.create({ ...body, password });
+  }
+
+  @Get(':id')
+  async get(@Param('id') id: string) {
+    return this.userService.findOne({ id });
   }
 }
