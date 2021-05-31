26321a8ba8cf9c23495511805f29c1aa01eee12a
Martin <mselvenis@gmail.com>
2021-05-31 19:42:27 +0100

    23. Permissions

---
+++ src/permission/permission.controller.ts
@@ -0,0 +1,11 @@
+import { PermissionService } from './permission.service';
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('permissions')
+export class PermissionController {
+  constructor(private permissionService: PermissionService) {}
+  @Get()
+  async all() {
+    return this.permissionService.all();
+  }
+}
